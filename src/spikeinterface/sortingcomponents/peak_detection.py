da6afd58e0d1b2c3309c50095f49e3db927da797
Sebastien <pierre.yger@gmail.com>
2024-09-27 13:49:40 +0200

    WIP

--- src/spikeinterface/sortingcomponents/peak_detection.py
+++ src/spikeinterface/sortingcomponents/peak_detection.py
@@ -614,10 +614,11 @@ class DetectPeakMatchedFiltering(PeakDetector):
         detect_threshold=5,
         exclude_sweep_ms=0.1,
         radius_um=50,
-        rank=1,
+        rank=3,
         noise_levels=None,
         random_chunk_kwargs={"num_chunks_per_segment": 5},
         weight_method={},
+        optimize=True
     ):
         PeakDetector.__init__(self, recording, return_output=True)
 
@@ -627,8 +628,9 @@ class DetectPeakMatchedFiltering(PeakDetector):
         self.exclude_sweep_size = int(exclude_sweep_ms * recording.get_sampling_frequency() / 1000.0)
         channel_distance = get_channel_distances(recording)
         self.neighbours_mask = channel_distance <= radius_um
-
+        self.optimize = optimize
         self.conv_margin = prototype.shape[0]
+        self.prototype = prototype
 
         assert peak_sign in ("both", "neg", "pos")
         idx = np.argmax(np.abs(prototype))
@@ -644,32 +646,38 @@ class DetectPeakMatchedFiltering(PeakDetector):
         self.nbefore = int(ms_before * recording.sampling_frequency / 1000)
         contact_locations = recording.get_channel_locations()
         dist = np.linalg.norm(contact_locations[:, np.newaxis] - contact_locations[np.newaxis, :], axis=2)
-        weights, self.z_factors = get_convolution_weights(dist, **weight_method)
+        self.weights, self.z_factors = get_convolution_weights(dist, **weight_method)
 
         num_channels = recording.get_num_channels()
         num_templates = num_channels * len(self.z_factors)
-        weights = weights.reshape(num_templates, -1)
+        self.weights = self.weights.reshape(num_templates, -1)
+        
+        if not self.optimize:
+            templates = self.weights[:, None, :] * self.prototype[None, :, None]
+            templates -= templates.mean(axis=(1, 2))[:, None, None]
+            temporal, singular, spatial = np.linalg.svd(templates, full_matrices=False)
+            temporal = temporal[:, :, :rank]
 
-        templates = weights[:, None, :] * prototype[None, :, None]
-        templates -= templates.mean(axis=(1, 2))[:, None, None]
-        temporal, singular, spatial = np.linalg.svd(templates, full_matrices=False)
-        temporal = temporal[:, :, :rank]
-        singular = singular[:, :rank]
-        spatial = spatial[:, :rank, :]
-        templates = np.matmul(temporal * singular[:, np.newaxis, :], spatial)
-        norms = np.linalg.norm(templates, axis=(1, 2))
-        del templates
+            singular = singular[:, :rank]
+            spatial = spatial[:, :rank, :]
+            templates = np.matmul(temporal * singular[:, np.newaxis, :], spatial)
+            norms = np.linalg.norm(templates, axis=(1, 2))
+            del templates
 
-        temporal /= norms[:, np.newaxis, np.newaxis]
-        temporal = np.flip(temporal, axis=1)
-        spatial = np.moveaxis(spatial, [0, 1, 2], [1, 0, 2])
-        temporal = np.moveaxis(temporal, [0, 1, 2], [1, 2, 0])
-        singular = singular.T[:, :, np.newaxis]
+            temporal /= norms[:, np.newaxis, np.newaxis]
+            temporal = np.flip(temporal, axis=1)
+            spatial = np.moveaxis(spatial, [0, 1, 2], [1, 0, 2])
+            temporal = np.moveaxis(temporal, [0, 1, 2], [1, 2, 0])
+            singular = singular.T[:, :, np.newaxis]
 
-        self.temporal = temporal
-        self.spatial = spatial
-        self.singular = singular
-
+            self.temporal = temporal.astype(np.float32)
+            self.spatial = spatial.astype(np.float32)
+            self.singular = singular.astype(np.float32)
+            self.num_templates = self.temporal.shape[1]
+        else:
+            self.prototype = np.flip(self.prototype)/np.linalg.norm(self.prototype)
+            self.num_templates = num_templates
+        
         random_data = get_random_data_chunks(recording, return_scaled=False, **random_chunk_kwargs)
         conv_random_data = self.get_convolved_traces(random_data, temporal, spatial, singular)
         medians = np.median(conv_random_data, axis=1)
@@ -741,14 +749,17 @@ class DetectPeakMatchedFiltering(PeakDetector):
 
     def get_convolved_traces(self, traces, temporal, spatial, singular):
         import scipy.signal
-
-        num_timesteps, num_templates = len(traces), temporal.shape[1]
-        num_peaks = num_timesteps - self.conv_margin + 1
-        scalar_products = np.zeros((num_templates, num_peaks), dtype=np.float32)
-        spatially_filtered_data = np.matmul(spatial, traces.T[np.newaxis, :, :])
-        scaled_filtered_data = spatially_filtered_data * singular
-        objective_by_rank = scipy.signal.oaconvolve(scaled_filtered_data, temporal, axes=2, mode="valid")
-        scalar_products += np.sum(objective_by_rank, axis=0)
+        if not self.optimize:
+            num_timesteps = len(traces)
+            num_peaks = num_timesteps - self.conv_margin + 1
+            scalar_products = np.zeros((self.num_templates, num_peaks), dtype=np.float32)
+            spatially_filtered_data = np.matmul(self.spatial, traces.T[np.newaxis, :, :])
+            scaled_filtered_data = spatially_filtered_data * self.singular
+            objective_by_rank = scipy.signal.oaconvolve(scaled_filtered_data, self.temporal, axes=2, mode="valid")
+            scalar_products += np.sum(objective_by_rank, axis=0)
+        else:
+            tmp = scipy.signal.oaconvolve(self.prototype[None, :], traces.T, axes=1, mode="valid")
+            scalar_products = np.dot(self.weights, tmp)
         return scalar_products
 
 
