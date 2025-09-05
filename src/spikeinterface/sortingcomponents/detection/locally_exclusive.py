d25df427dec6caeae253a764ce70c89b19c9e24a
Sebastien <pierre.yger@gmail.com>
2025-09-05 14:01:46 +0200

    WIP

--- src/spikeinterface/sortingcomponents/detection/locally_exclusive.py
+++ src/spikeinterface/sortingcomponents/detection/locally_exclusive.py
@@ -169,7 +169,9 @@ if HAVE_NUMBA:
                     if not peak_mask[s, chan_ind]:
                         break
         return peak_mask
-    
+
+
+
 class LocallyExclusiveTorchPeakDetector(ByChannelTorchPeakDetector):
     """Detect peaks using the "locally exclusive" method with pytorch."""
 
@@ -213,9 +215,10 @@ class LocallyExclusiveTorchPeakDetector(ByChannelTorchPeakDetector):
 
         self.channel_distance = get_channel_distances(recording)
         self.neighbour_indices_by_chan = []
+        self.radius_um = radius_um
         self.num_channels = recording.get_num_channels()
         for chan in range(self.num_channels):
-            self.neighbour_indices_by_chan.append(np.nonzero(self.channel_distance[chan] <= radius_um)[0])
+            self.neighbour_indices_by_chan.append(np.nonzero(self.channel_distance[chan] <= self.radius_um)[0])
         self.max_neighbs = np.max([len(neigh) for neigh in self.neighbour_indices_by_chan])
         self.neighbours_idxs = self.num_channels * np.ones((self.num_channels, self.max_neighbs), dtype=int)
         for i, neigh in enumerate(self.neighbour_indices_by_chan):
@@ -239,3 +242,255 @@ class LocallyExclusiveTorchPeakDetector(ByChannelTorchPeakDetector):
             local_peaks["amplitude"] = peak_amplitude
             local_peaks["segment_index"] = segment_index
         return (local_peaks, )
+
+
+
+class LocallyExclusiveOpenCLPeakDetector(LocallyExclusivePeakDetector):
+    name = "locally_exclusive_cl"
+    engine = "opencl"
+    preferred_mp_context = None
+    #params_doc = (
+    #    DetectPeakLocallyExclusive.params_doc
+    #    + """
+    #opencl_context_kwargs: None or dict
+    #    kwargs to create the opencl context
+    #"""
+    #)
+
+
+    def __init__(
+        self,
+        recording,
+        peak_sign="neg",
+        detect_threshold=5,
+        exclude_sweep_ms=0.1,
+        radius_um=50,
+        noise_levels=None,
+        random_chunk_kwargs={},
+    ):
+        LocallyExclusivePeakDetector.__init__(self, 
+                                            recording,
+                                            peak_sign,
+                                            detect_threshold,
+                                            exclude_sweep_ms,
+                                            radius_um,
+                                            noise_levels,
+                                            random_chunk_kwargs)
+
+        self.executor = OpenCLDetectPeakExecutor(self.abs_thresholds, 
+                                            self.exclude_sweep_size, 
+                                            self.neighbours_mask, 
+                                            self.peak_sign)
+
+
+    def compute(self, traces, start_frame, end_frame, segment_index, max_margin):
+        peak_sample_ind, peak_chan_ind = self.executor.detect_peak(traces)
+        peak_sample_ind += self.exclude_sweep_size
+        peak_amplitude = traces[peak_sample_ind, peak_chan_ind]
+
+        local_peaks = np.zeros(peak_sample_ind.size, dtype=self.get_dtype())
+        local_peaks["sample_index"] = peak_sample_ind
+        local_peaks["channel_index"] = peak_chan_ind
+        local_peaks["amplitude"] = peak_amplitude
+        local_peaks["segment_index"] = segment_index
+
+        return (local_peaks, )
+
+
+class OpenCLDetectPeakExecutor:
+    def __init__(self, abs_thresholds, exclude_sweep_size, neighbours_mask, peak_sign):
+
+        self.chunk_size = None
+
+        self.abs_thresholds = abs_thresholds.astype("float32")
+        self.exclude_sweep_size = exclude_sweep_size
+        self.neighbours_mask = neighbours_mask.astype("uint8")
+        self.peak_sign = peak_sign
+        self.ctx = None
+        self.queue = None
+        self.x = 0
+
+    def create_buffers_and_compile(self, chunk_size):
+        import pyopencl
+
+        mf = pyopencl.mem_flags
+        try:
+            self.device = pyopencl.get_platforms()[0].get_devices()[0]
+            self.ctx = pyopencl.Context(devices=[self.device])
+        except Exception as e:
+            print("error create context ", e)
+
+        self.queue = pyopencl.CommandQueue(self.ctx)
+        self.max_wg_size = self.ctx.devices[0].get_info(pyopencl.device_info.MAX_WORK_GROUP_SIZE)
+        self.chunk_size = chunk_size
+
+        self.neighbours_mask_cl = pyopencl.Buffer(
+            self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.neighbours_mask
+        )
+        self.abs_thresholds_cl = pyopencl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.abs_thresholds)
+
+        num_channels = self.neighbours_mask.shape[0]
+        self.traces_cl = pyopencl.Buffer(self.ctx, mf.READ_WRITE, size=int(chunk_size * num_channels * 4))
+
+        # TODO estimate smaller
+        self.num_peaks = np.zeros(1, dtype="int32")
+        self.num_peaks_cl = pyopencl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.num_peaks)
+
+        nb_max_spike_in_chunk = num_channels * chunk_size
+        self.peaks = np.zeros(nb_max_spike_in_chunk, dtype=[("sample_index", "int32"), ("channel_index", "int32")])
+        self.peaks_cl = pyopencl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.peaks)
+
+        variables = dict(
+            chunk_size=int(self.chunk_size),
+            exclude_sweep_size=int(self.exclude_sweep_size),
+            peak_sign={"pos": 1, "neg": -1}[self.peak_sign],
+            num_channels=num_channels,
+        )
+
+        kernel_formated = processor_kernel % variables
+        prg = pyopencl.Program(self.ctx, kernel_formated)
+        self.opencl_prg = prg.build()  # options='-cl-mad-enable'
+        self.kern_detect_peaks = getattr(self.opencl_prg, "detect_peaks")
+
+        self.kern_detect_peaks.set_args(
+            self.traces_cl, self.neighbours_mask_cl, self.abs_thresholds_cl, self.peaks_cl, self.num_peaks_cl
+        )
+
+        s = self.chunk_size - 2 * self.exclude_sweep_size
+        self.global_size = (s,)
+        self.local_size = None
+
+    def detect_peak(self, traces):
+        self.x += 1
+
+        import pyopencl
+
+        if self.chunk_size is None or self.chunk_size != traces.shape[0]:
+            self.create_buffers_and_compile(traces.shape[0])
+        event = pyopencl.enqueue_copy(self.queue, self.traces_cl, traces.astype("float32"))
+
+        pyopencl.enqueue_nd_range_kernel(
+            self.queue,
+            self.kern_detect_peaks,
+            self.global_size,
+            self.local_size,
+        )
+
+        event = pyopencl.enqueue_copy(self.queue, self.traces_cl, traces.astype("float32"))
+        event = pyopencl.enqueue_copy(self.queue, self.traces_cl, traces.astype("float32"))
+        event = pyopencl.enqueue_copy(self.queue, self.num_peaks, self.num_peaks_cl)
+        event = pyopencl.enqueue_copy(self.queue, self.peaks, self.peaks_cl)
+        event.wait()
+
+        n = self.num_peaks[0]
+        peaks = self.peaks[:n]
+        peak_sample_ind = peaks["sample_index"].astype("int64")
+        peak_chan_ind = peaks["channel_index"].astype("int64")
+
+        return peak_sample_ind, peak_chan_ind
+
+
+processor_kernel = """
+#define chunk_size %(chunk_size)d
+#define exclude_sweep_size %(exclude_sweep_size)d
+#define peak_sign %(peak_sign)d
+#define num_channels %(num_channels)d
+
+
+typedef struct st_peak{
+    int sample_index;
+    int channel_index;
+} st_peak;
+
+
+__kernel void detect_peaks(
+                        //in
+                        __global  float *traces,
+                        __global  uchar *neighbours_mask,
+                        __global  float *abs_thresholds,
+                        //out
+                        __global  st_peak *peaks,
+                        volatile __global int *num_peaks
+                ){
+    int pos = get_global_id(0);
+
+    if (pos == 0){
+        *num_peaks = 0;
+    }
+    // this barrier OK if the first group is run first
+    barrier(CLK_GLOBAL_MEM_FENCE);
+
+    if (pos>=(chunk_size - (2 * exclude_sweep_size))){
+        return;
+    }
+
+
+    float v;
+    uchar peak;
+    uchar is_neighbour;
+
+    int index;
+
+    int i_peak;
+
+
+    for (int chan=0; chan<num_channels; chan++){
+
+        //v = traces[(pos + exclude_sweep_size)*num_channels + chan];
+        index = (pos + exclude_sweep_size) * num_channels + chan;
+        v = traces[index];
+
+        if(peak_sign==1){
+            if (v>abs_thresholds[chan]){peak=1;}
+            else {peak=0;}
+        }
+        else if(peak_sign==-1){
+            if (v<-abs_thresholds[chan]){peak=1;}
+            else {peak=0;}
+        }
+
+        if (peak == 1){
+            for (int chan_neigh=0; chan_neigh<num_channels; chan_neigh++){
+
+                is_neighbour = neighbours_mask[chan * num_channels + chan_neigh];
+                if (is_neighbour == 0){continue;}
+                //if (chan == chan_neigh){continue;}
+
+                index = (pos + exclude_sweep_size) * num_channels + chan_neigh;
+                if(peak_sign==1){
+                    peak = peak && (v>=traces[index]);
+                }
+                else if(peak_sign==-1){
+                    peak = peak && (v<=traces[index]);
+                }
+
+                if (peak==0){break;}
+
+                if(peak_sign==1){
+                    for (int i=1; i<=exclude_sweep_size; i++){
+                        peak = peak && (v>traces[(pos + exclude_sweep_size - i)*num_channels + chan_neigh]) && (v>=traces[(pos + exclude_sweep_size + i)*num_channels + chan_neigh]);
+                        if (peak==0){break;}
+                    }
+                }
+                else if(peak_sign==-1){
+                    for (int i=1; i<=exclude_sweep_size; i++){
+                        peak = peak && (v<traces[(pos + exclude_sweep_size - i)*num_channels + chan_neigh]) && (v<=traces[(pos + exclude_sweep_size + i)*num_channels + chan_neigh]);
+                        if (peak==0){break;}
+                    }
+                }
+
+            }
+
+        }
+
+        if (peak==1){
+            //append to
+            i_peak = atomic_inc(num_peaks);
+            // sample_index is LOCAL to fifo
+            peaks[i_peak].sample_index = pos + exclude_sweep_size;
+            peaks[i_peak].channel_index = chan;
+        }
+    }
+
+}
+"""
--- src/spikeinterface/sortingcomponents/detection/method_list.py
+++ src/spikeinterface/sortingcomponents/detection/method_list.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 
-from .iterative import IterativePeakDetector
 from .matched_filtering import MatchedFilteringPeakDetector
 from .by_channel import ByChannelPeakDetector, ByChannelTorchPeakDetector
-from .locally_exclusive import LocallyExclusivePeakDetector, LocallyExclusiveTorchPeakDetector
+from .locally_exclusive import LocallyExclusivePeakDetector, LocallyExclusiveTorchPeakDetector, LocallyExclusiveOpenCLPeakDetector
 
 detection_methods = {
     "locally_exclusive": LocallyExclusivePeakDetector,
     "locally_exclusive_torch": LocallyExclusiveTorchPeakDetector,
+    "locally_exclusive_cl": LocallyExclusiveOpenCLPeakDetector,
     "matched_filtering": MatchedFilteringPeakDetector,
-    "iterative": IterativePeakDetector,
     "by_channel": ByChannelPeakDetector,
     "by_channel_torch": ByChannelTorchPeakDetector
 }
